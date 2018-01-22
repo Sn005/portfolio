60a7c34661ccdf11a0129f3937abc516954c5c78
narihara <narihara@webnear.co.jp>
2018-01-22 13:08:44 +0900

    mod typo

--- plugins/user.js
+++ plugins/user.js
@@ -2,8 +2,8 @@ import users from '~/api/firebase/users'
 
 async function user ({ store }) {
   const result = await users.show()
-  store.dispatch('user/setAccount', result.account)
   store.dispatch('user/setAuth', result.auth)
+  store.dispatch('user/setAccount', result.account)
 }
 
 export default user
