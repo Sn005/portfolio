4514be000ba32dabd08752abe8b634c8d6f650ce
narihara <narihara@webnear.co.jp>
2018-01-23 19:29:17 +0900

    mod 一旦仮組

--- middleware/is-authed.js
+++ middleware/is-authed.js
@@ -1,5 +1,9 @@
-export default function ({ store, redirect, error }) {
-  if (store.state.user.auth) {
-    return redirect('/admin')
-  }
+import { auth } from '~/api/firebase/partial/auth'
+export default async function ({ redirect }) {
+  const isAuthed = await auth()
+  // if (!isAuthed) {
+  //   return redirect('/auth/login')
+  // } else {
+  //   return redirect('/admin')
+  // }
 }
