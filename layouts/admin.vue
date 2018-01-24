c1b7d9b966fca202d8b7ca27ed791149b1d90c63
narihara <narihara@webnear.co.jp>
2018-01-24 13:04:30 +0900

    change admin/layout

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -19,17 +19,6 @@
             v-icon(v-html="item.icon")
           v-list-tile-content
             v-list-tile-title(v-text="item.title")
-    v-toolbar(
-      fixed
-      app
-      :clipped-left="clipped"
-    )
-      v-toolbar-side-icon(@click="drawer = !drawer")
-      v-btn(
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      )
-        v-icon(v-html="miniVariant ? 'chevron_right' : 'chevron_left'")
     v-content
       v-container
         nuxt
