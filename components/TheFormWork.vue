b2c2f60d22374b0885d581345c9f6e0ed366418d
snasna <wisperinthedustbox@gmail.com>
2018-01-25 00:59:24 +0900

    create sass/moudles

---
+++ components/TheFormWork.vue
@@ -0,0 +1,58 @@
+<template lang='pug'>
+  v-form.pa-4(v-model="valid")
+    v-text-field(
+      label="Name"
+      v-model="name"
+      :rules="nameRules"
+      :counter="20"
+      required
+    )
+    v-layout(row)
+      v-flex(
+        xs6
+        md2
+      )
+        v-checkbox(
+          label="wordpress"
+          v-model="category"
+          value="wordpress"
+        )
+      v-flex(
+        xs6
+        md2
+      )
+        v-checkbox(
+          label="hoge"
+          v-model="category"
+          value="hoge"
+        )
+    div.editor-wrapper
+      vue-editor(v-model="content")
+</template>
+<script>
+import { VueEditor } from 'vue2-editor'
+export default {
+  components: {VueEditor},
+  props: {
+    item: Object
+  },
+  data () {
+    return {
+      valid: false,
+      name: this.item.name,
+      category: this.item.category,
+      content: this.item.content,
+      nameRules: [
+        (v) => !!v || 'Name is required',
+        (v) => v.length <= 20 || 'Name must be less than 10 characters'
+      ]
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+@import "../assets/style/scss/modules/_m-editor.scss";
+.editor-wrapper{
+  @extend %m-editor-wrapper;
+}
+</style>
--- pages/admin/works/_id.vue
+++ pages/admin/works/_id.vue
@@ -2,72 +2,34 @@
   v-layout(column)
     v-card
       v-toolbar
-        v-toolbar-title {{ currentName }}
-      v-form.pa-4(v-model="valid")
-        v-text-field(
-          label="Name"
-          v-model="name"
-          :rules="nameRules"
-          :counter="20"
-          required
-        )
-        v-layout(row)
-          v-flex(
-            xs6
-            md2
-          )
-            v-checkbox(
-              label="wordpress"
-              v-model="category"
-              value="wordpress"
-            )
-          v-flex(
-            xs6
-            md2
-          )
-            v-checkbox(
-              label="hoge"
-              v-model="category"
-              value="hoge"
-            )
-        div.editor-wrapper
-          vue-editor(v-model="content")
+        v-toolbar-title {{ name }}
+      the-form-work(:item="item")
 </template>
 <script>
 import { item as firebaseWorksItem } from '~/api/firebase/works'
-import { VueEditor } from 'vue2-editor'
+import TheFormWork from '~/components/TheFormWork'
+// import { VueEditor } from 'vue2-editor'
 export default {
   layout: 'admin',
   async asyncData ({ params, error }) {
-    const item = await firebaseWorksItem(params.id)
+    let item = await firebaseWorksItem(params.id)
+    item['category'] = Object.keys(item['category'])
     return {
-      currentName: item.name,
       name: item.name,
-      content: item.content,
-      category: Object.keys(item.category)
+      item: item
     }
   },
   data () {
     return {
-      valid: false,
-      name: '',
-      nameRules: [
-        (v) => !!v || 'Name is required',
-        (v) => v.length <= 20 || 'Name must be less than 10 characters'
-      ],
-      category: [],
-      content: ''
+      item: {
+        name: '',
+        category: [],
+        content: ''
+      }
     }
   },
   components: {
-    VueEditor
+    TheFormWork
   }
 }
 </script>
-<style scoped lang="scss">
-.editor{
-  &-wrapper{
-    background : #1d1d1d;
-  }
-}
-</style>
