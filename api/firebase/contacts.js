8ca4b8b67a463214b973b7934f8e595d82892409
snasna <wisperinthedustbox@gmail.com>
2018-01-28 16:19:26 +0900

    fix contacts/

---
+++ api/firebase/contacts.js
@@ -0,0 +1,56 @@
+import firebase from '~/plugins/firebase'
+
+const db = firebase.firestore()
+const col = db.collection('contacts')
+
+/**
+ * 一覧情報取得
+ */
+export const items = async () => {
+  const snapshot = await col.get()
+  return snapshot.docs.map(function (doc) {
+    let data = doc.data()
+    data['id'] = doc.id
+    return data
+  })
+}
+
+/**
+ * 単体情報取得
+ */
+export const item = async (id) => {
+  const item = await col.doc(id).get().catch(error => {
+    console.log(error)
+  })
+  if (!item.exists) return false
+  let result = item.data()
+  result.id = id
+  return result
+}
+
+/**
+ * 情報をfirebaseに送信する
+ * @param {string} id 対象記事のID
+ * @param {objext} payload 送信情報
+ */
+export const send = async (id, payload) => {
+  await col.doc(id).set(payload).catch(error => {
+    console.log(error)
+  })
+}
+
+/**
+ * 対象アイテムを削除する
+ * @param {string} id 対象記事のID
+ */
+export const remove = async (id) => {
+  const doc = col.doc(id)
+  const item = await doc.get().catch(error => {
+    console.log(error)
+  })
+  if (!item.exists) return false
+  await doc.delete().catch(error => {
+    console.log(error)
+  })
+  return true
+}
--- pages/admin/contacts/index.vue
+++ pages/admin/contacts/index.vue
@@ -1,139 +1,48 @@
 <template lang="pug">
   v-layout(column)
     v-toolbar
-      v-toolbar-title Works
-      v-spacer
-      v-btn(
-        icon
-        @click.native.stop="dialog.sort = true"
-      )
-        v-icon sort
-      v-btn(
-        icon
-        to="works/create"
-      )
-        v-icon note_add
+      v-toolbar-title Contacts
     v-data-table(
       :headers="headers"
       :items="items"
-      hide-actions
-      class="elevation-1"
+      class="elevation-4"
     )
       template(
         slot="items"
         slot-scope="props"
       )
-        td {{ props.item.name }}
-        td {{ props.item.id }}
-        td.text-xs-right {{ props.item.order }}
+        td {{ props.item.title }}
+        td.text-xs-right {{ props.item.created }}
         td.text-xs-right
           v-btn(
             flat
             icon
             color="purple lighten-3"
-            :to="'works/' + props.item.id"
+            :to="'contacts/' + props.item.id"
           )
-            v-icon edit
-          v-btn(
-            flat
-            icon
-            color="red lighten-1"
-            @click.native.stop="dialog.delete = true"
-          )
-            v-icon delete
-        v-dialog(
-          v-model="dialog.delete"
-          persistent
-          max-width="290"
-        )
-          v-card
-            v-card-title このアイテムを削除しますか？
-            v-card-actions
-              v-spacer
-              v-btn(
-                flat
-                @click.native="dialog.delete = false"
-              ) いいえ
-              v-btn(
-                flat
-                @click="remove(props.item.id)"
-              ) はい
-    v-dialog(
-      v-model="dialog.sort"
-      persistent
-      scrollable
-    )
-      v-card
-        v-toolbar
-          v-toolbar-title 表示順変更
-          v-spacer
-          v-btn(
-            icon
-            @click.native="dialog.sort = false"
-          )
-            v-icon clear
-        v-progress-linear.mt-0(
-          v-if="isSend"
-          :indeterminate="true"
-        )
-        v-list
-          draggable(
-            :list = "items"
-            @end="dragEnd"
-          )
-            v-list-tile.px-4(
-              v-for="item in items"
-              @click=""
-            )
-              v-list-tile-content(v-text="item.name")
-              v-list-tile-action
-                v-icon open_with
+            v-icon redo
 </template>
 <script>
-import {
-  items as firebaseWorksItems,
-  send as firebaseWorksSend,
-  remove as firebaseWorksRemove
-} from '~/api/firebase/works'
-import draggable from 'vuedraggable'
+import * as firebaseContacts from '~/api/firebase/contacts'
 export default {
   layout: 'admin',
   async asyncData () {
-    const items = await firebaseWorksItems()
     return {
-      items: items.sort((a, b) => {
-        if (a.order < b.order) return -1
-        if (a.order > b.order) return 1
-        return 0
-      })
+      items: await firebaseContacts.items()
     }
-  },
-  components: {
-    draggable
   },
   data () {
     return {
-      isSend: false,
-      dialog: {
-        delete: false,
-        sort: false
-      },
       headers: [
         {
-          text: 'Name',
-          value: 'name',
+          text: 'Title',
+          value: 'title',
           align: 'left',
           sortable: false
         },
         {
-          text: 'Id',
-          value: 'id',
-          align: 'left',
-          sortable: false
-        },
-        {
-          text: '表示順',
-          value: 'order'
+          text: '投稿日',
+          value: 'created'
         },
         {
           text: 'Action',
@@ -141,28 +50,6 @@ export default {
           sortable: false
         }
       ]
-    }
-  },
-  methods: {
-    async remove (id) {
-      const result = await firebaseWorksRemove(id)
-      if (!result) return
-      this.dialog.delete = false
-      this.items = this.items.filter(item => {
-        return item.id !== id
-      })
-    },
-    async dragEnd () {
-      this.isSend = true
-      const newItems = this.items.map((item, index) => {
-        item.order = index + 1
-        return item
-      })
-      for (let item of newItems) {
-        await firebaseWorksSend(item.id, item)
-      }
-      this.isSend = false
-      this.items = newItems
     }
   }
 }
