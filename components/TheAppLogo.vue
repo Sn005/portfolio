dc8b6e45429c47369cb6cea0977e1ad2f869a5cc
snasna <wisperinthedustbox@gmail.com>
2018-01-31 01:48:26 +0900

    create the logo component

---
+++ components/TheAppLogo.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  div.app-logo
+    section.app-logo__content
+      h1.app-logo__title narihara portfolio
+      img(src="/images/icon-app.png" width="180px")
+</template>
+<style lang="scss">
+@import "../assets/style/scss/_all";
+
+.app-logo{
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  &__title{
+    font-size: 2rem;
+    color: $true-white;
+  }
+  &__content{
+    text-align: center;
+    @include set-mid-mid();
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,40 @@
     id="app"
   )
     div.app-body
+      the-app-logo
 </template>
+<script>
+import TheAppLogo from '~/components/TheAppLogo'
+export default {
+  components: {
+    TheAppLogo
+  }
+}
+</script>
 <style lang="scss">
-@import "../assets/style/scss/variables/_themes";
+@import "../assets/style/scss/variables/_all";
 @import "../assets/style/scss/modules/_m-app-body";
 #app{
   @extend %m-app-body;
   background : $bg-body;
+  animation: bg 10s $ease-in-out infinite;
+  &:after{
+    content: '';
+    width: 100vw;
+    height: 100vh;
+    background:  url(#{$dir-img}bg-body.png);
+    opacity: 0.035;
+  }
+}
+@keyframes bg {
+  0% {
+    background : $bg-body;
+  }
+  50% {
+    background :darken($darken-red , 24%);
+  }
+  100% {
+    background : $bg-body;
+  }
 }
 </style>
