a321a200b502c5b93269d951c08695ffa05ae153
snasna <wisperinthedustbox@gmail.com>
2018-01-31 23:15:14 +0900

    add the-app-logo to page/index

--- components/TheAppLogo.vue
+++ components/TheAppLogo.vue
@@ -1,22 +1,18 @@
 <template lang="pug">
   div.app-logo
     section.app-logo__content
-      h1.app-logo__title portfolio
-      //- img(src="/images/icon-app.png" width="180px")
+      h1.app-logo__title narihara portfolio
 </template>
 <style lang="scss">
 @import "../assets/style/scss/_all";
 
 .app-logo{
   position: relative;
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   &__title{
     font-family: 'Josefin Slab', serif;
     font-weight:100;
-    // font-family: 'Julius Sans One', sans-serif;
-    // font-family: 'Poiret One', cursive;
-
     letter-spacing: 0.1em;
     font-size: 2rem;
     color: $true-white;
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template lang="pug">
-
+  div
+    the-app-logo
 </template>
 <script>
-
+import TheAppLogo from '~/components/TheAppLogo'
+export default {
+  components: {
+    TheAppLogo
+  }
+}
 </script>
