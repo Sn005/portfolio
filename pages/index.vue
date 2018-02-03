94087c206133085454d0cdd81f2a370b4f123396
snasna <wisperinthedustbox@gmail.com>
2018-02-03 19:05:12 +0900

    add topページ下部装飾

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  div
+  div.page-index
     app-logo
 </template>
 <script>
@@ -10,3 +10,30 @@ export default {
   }
 }
 </script>
+<style scoped lang="scss">
+@import "../assets/style/scss/_all";
+$deg: 30deg !default;
+
+
+.page-index{
+  position: relative;
+  &:before,
+  &:after{
+    content: '';
+    position: fixed;
+    height: 100vh;
+    width: 100%;
+    bottom: -93vh;
+    background: darken($primary-white, 10%);
+    box-shadow: 0 4px 10px rgba($true-black, 0.3) inset;
+  }
+  &:before{
+    left: 0;
+    transform: skew($deg) rotate($deg);
+  }
+  &:after{
+    right: 0;
+    transform: skew(- $deg) rotate(- $deg);
+  }
+}
+</style>
