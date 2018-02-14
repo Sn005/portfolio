4e225166b60d071b0079cde13ed2ec9d15130c95
snasna <wisperinthedustbox@gmail.com>
2018-02-15 08:56:03 +0900

    fix page-title

--- components/ThePageTitleLower.vue
+++ components/ThePageTitleLower.vue
@@ -2,6 +2,8 @@
   header.page-title-lower
     h1.page-title-lower__text
       slot
+    div.page-title-lower__arrow--left
+    div.page-title-lower__arrow--right
 </template>
 <script>
 export default {
@@ -19,41 +21,12 @@ export default {
 <style scoped lang="scss">
 @import "../assets/style/scss/_all";
 .page-title-lower{
-  $deg: 30deg;
-  // $bg: darken($primary-white, 10%);
-  $bg: darken($primary-color, 30%);
-  $height: 120px;
-
-  position: relative;
-  width: 100%;
-  height: $height;
-  background: $bg;
-  margin-bottom: 88px;
-  &:before{
-    $size: 200px;
-    content: '';
-    position: absolute;
-    bottom: - ($size / 4) ;
-    left: 50%;
-    margin-left: -($size / 2);
-    width: $size;
-    height: $size;
-    background: $bg;
-    transform: rotate(45deg);
-    z-index: 1;
-  }
+  @include page-title();
   &__text{
     display: flex;
     align-items: center;
     position: relative;
     margin: 0 auto;
-    line-height: $height;
-    text-align: center;
-    color: $true-white;
-    z-index: index($z, lower-title);
-    opacity: 0;
-    animation: pageTitleText 1s 1s $easeOutQuad forwards;
-    @include text-title();
     @include mq-tabl-pcl{
       font-size: 2rem;
       width: 40%;
@@ -78,19 +51,6 @@ export default {
         margin:0 4px;
       }
     }
-  }
-}
-@keyframes pageTitleText {
-  0% {
-    opacity: 0;
-    transform: translate3d(0, 20px, 0);
-  }
-  50% {
-    opacity: 0.3;
-  }
-  100% {
-    opacity: 1;
-    transform: translate3d(0, 0, 0);
   }
 }
 </style>
--- components/ThePageTitleWork.vue
+++ components/ThePageTitleWork.vue
@@ -26,85 +26,11 @@ export default {
 <style scoped lang="scss">
 @import "../assets/style/scss/_all";
 .page-title-work{
-  $bg: $bg-body;
-  $headerH: 200px;
-  $arrowSize: 80px;
-  $textH: $headerH - $arrowSize;
-  $deg: 44deg;
-  $arrowSpacer: -3%;
-
-  position: relative;
-  width: 100%;
-  height: $headerH;
-  background: $bg;
-  margin-bottom: 88px;
-  &__arrow--left,
-  &__arrow--right{
-    position: absolute;
-    bottom: 0;
-    width: calc(50% - #{$arrowSize});
-    height: $arrowSize;
-    background: $bg;
-    z-index: 3;
-    &:after{
-      content: '';
-      position: absolute;
-      top: 0;
-      border: $arrowSize / 2 solid transparent;
-    }
-  }
-  &__arrow--left{
-    left: 0;
-    &:after{
-      right: - $arrowSize;
-      border-left-color: $bg;
-      border-bottom-color: $bg;
-    }
-  }
-  &__arrow--right{
-    right: 0;
-    &:after{
-      left: - $arrowSize;
-      border-right-color: $bg;
-      border-bottom-color: $bg;
-    }
-  }
+  @include page-title();
 
   &__text{
     background-size: cover;
-    line-height: $textH;
-    text-align: center;
-    color: $true-white;
-    z-index: index($z, lower-title);
-    opacity: 0;
-    animation: pageTitleText 1s 1s $easeOutQuad forwards;
     @include text-title();
-    // &:before{
-    //   $size: 200px;
-    //   content: '';
-    //   position: absolute;
-    //   bottom: -31px;
-    //   left: 50%;
-    //   margin-left: -($size / 2);
-    //   width: $size;
-    //   height: $size;
-    //   background: $bg;
-    //   transform: rotate(45deg);
-    //   z-index: 1;
-    // }
-  }
-}
-@keyframes pageTitleText {
-  0% {
-    opacity: 0;
-    transform: translate3d(0, 20px, 0);
-  }
-  50% {
-    opacity: 0.3;
-  }
-  100% {
-    opacity: 1;
-    transform: translate3d(0, 0, 0);
   }
 }
 </style>
