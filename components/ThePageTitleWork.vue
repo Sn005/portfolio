08ec824ff0965dab9c7279ba21007511f146a068
narihara <narihara@webnear.co.jp>
2018-02-15 16:03:51 +0900

    mod worksタイトル背景調整

--- components/ThePageTitleWork.vue
+++ components/ThePageTitleWork.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
-  header.page-title-work(:style="styleObject")
+  header.page-title-work
     h1.page-title-work__text {{ item.name }}
+    div.page-title-work__inner.page-title-work(:style="styleObject")
     div.page-title-work__arrow--left
     div.page-title-work__arrow--right
 </template>
@@ -30,6 +31,14 @@ export default {
   // page-transitionの際に背景画像が線のようにチラつくので
   // 下記border設定で塗りつぶし対処
   border-bottom: 10px solid $bg-body;
+  &__inner{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: calc(100% + 10px);
+    opacity: 0.4;
+  }
   &__text{
     background-size: cover;
     @include text-title();
