57bef6c36fab7ffea5313535d912167820bc7fb4
snasna <wisperinthedustbox@gmail.com>
2018-02-17 18:51:13 +0900

    create /about

---
+++ pages/about.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  div
+    the-page-title-lower
+      | ABOUT
+    v-content
+      v-container
+        card-article
+          v-flex(
+            md10
+            xs12
+            offset-md1
+          )
+            card-article-content
+              p {{ item.content }}
+</template>
+<script>
+
+</script>
+<script>
+import ThePageTitleLower from '~/components/ThePageTitleLower'
+import CardArticle from '~/components/CardArticle'
+import CardArticleContent from '~/components/CardArticleContent'
+import * as firebaseAbout from '~/api/firebase/about'
+export default {
+  components: {
+    ThePageTitleLower,
+    CardArticle,
+    CardArticleContent
+  },
+  async asyncData () {
+    return {
+      item: await firebaseAbout.item()
+    }
+  }
+}
+</script>
