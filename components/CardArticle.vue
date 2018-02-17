e1e293ca00e757b60642c403b91e25ede1783bdd
snasna <wisperinthedustbox@gmail.com>
2018-02-17 18:58:57 +0900

    mod card-artcileにアニメーション追加

--- components/CardArticle.vue
+++ components/CardArticle.vue
@@ -7,6 +7,8 @@
 .card-article{
   background: $primary-white;
   box-shadow:0 3px 2px 1px rgba($true-black, 0.9);
+  opacity: 0;
+  animation: cardArticle 1s 1.5s $easeOutQuad forwards;
   @include mq-tab-sp{
     padding: 16px;
   }
@@ -14,4 +16,14 @@
     padding: 24px;
   }
 }
+  @keyframes cardArticle {
+    0% {
+      opacity: 0;
+      transform: translate3d(0, 20px, 0);
+    }
+    100% {
+      opacity: 1;
+      transform: translate3d(0, 0, 0);
+    }
+  }
 </style>
