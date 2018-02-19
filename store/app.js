ad2ebde8f659f5895b804fe8c78cf8eb49b762d2
snasna <wisperinthedustbox@gmail.com>
2018-02-20 00:32:19 +0900

    clean 不要store削除

--- store/app.js
+++ store/app.js
@@ -1,27 +1,19 @@
 export const state = () => ({
-  isDrawer: false,
-  isVisit: false
+  isDrawer: false
 })
 
 export const mutations = {
   toggleDrawer (state) {
     state.isDrawer = !state.isDrawer
-  },
-  setVisit (state) {
-    state.isVisit = true
   }
 }
 
 export const actions = {
   toggleDrawer ({ commit }) {
     commit('toggleDrawer')
-  },
-  setVisit ({ commit }) {
-    commit('setVisit')
   }
 }
 
 export const getters = {
-  isDrawer: state => state.isDrawer,
-  isVisit: state => state.isVisit
+  isDrawer: state => state.isDrawer
 }
