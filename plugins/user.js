5d29ee7de444e3255cc319ac2129659bcbc06fd5
narihara <narihara@webnear.co.jp>
2018-02-20 17:56:12 +0900

    del 不要ファイル削除

--- plugins/user.js
+++
@@ -1,8 +0,0 @@
-import * as firebaseUsers from '~/api/firebase/users'
-async function user ({ store }) {
-  const result = await firebaseUsers.item()
-  store.dispatch('user/setAuth', result.auth)
-  store.dispatch('user/setAccount', result.account)
-}
-
-export default user
